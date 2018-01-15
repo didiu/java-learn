e12c66ad99143eabffdbdd6d597405dd3db38c32
Guangtu Xiao (Wicresoft North America Ltd) <v-guax@microsoft.com>
2018-01-15 20:26:42 +0800

    add Arranging_Coins

---
+++ LeetCodeTest/easy/Arranging_CoinsTest.java
@@ -0,0 +1,14 @@
+package easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Arranging_CoinsTest{
+
+  @Test
+  public void testArranging_Coins(){
+    assertEquals(1,new Arranging_Coins().arrangingCoins(1));
+    assertEquals(4,new Arranging_Coins().arrangingCoins(11));
+  }
+}
